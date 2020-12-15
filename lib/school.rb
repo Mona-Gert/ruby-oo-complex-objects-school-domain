f0dab3affe383dba8de420a57de590e352972e7f
monamohamed <mona.m.mohamed@gmail.com>
2020-12-15 14:25:00 -0500

    commit tree 188ccc5baa029a2e781d9dd4ce1ae8b4db854523

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+class School
+attr_reader :name
+
+def initialize(name)
+    @name=name
+    @roster={}
+end
+
+def roster
+    @roster
+end
+
+def add_student(name,grade)
+if @roster.has_key?(grade)
+@roster[grade]<<name
+else @roster[grade]=[name]
+end
+
+def grade(grade)
+    @roster[grade]
+
+end
+
+def sort 
+    self.roster.each do |g,s|
+        s.sort!
+    end
+end
+
+
+end
+
+end
